f61ff4633b65c7bf3354799dc761d0f3a496b405
Ahmed Adel Hassan <aa3452000@gmail.com>
2025-09-06 10:53:27 +0300

    V05

--- EF03/Program.cs
+++ EF03/Program.cs
@@ -1,5 +1,6 @@
 ﻿using EF03.Contexts;
 using EF03.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EF03
 {
@@ -36,6 +37,18 @@ namespace EF03
 
             //var employee = context.Employees.FirstOrDefault(e => e.Id == 10);
 
+            //Console.WriteLine(employee?.Id ?? 0);
+            //Console.WriteLine(employee?.Name ?? "NA");
+            //Console.WriteLine(employee?.Salary ?? 0.0f);
+            //Console.WriteLine(employee?.Address ?? "NA");
+            //Console.WriteLine(employee?.DeptId ?? 0);
+            //Console.WriteLine(employee?.HiringDate ?? DateTime.Now);
+            //Console.WriteLine(employee?.WorkFor?.Name ?? "NA");
+
+            // 1. Explicit Loading  
+
+            // var employee = context.Employees.FirstOrDefault(e => e.Id == 10);
+
             //context.Entry(employee).Reference("WorkFor").Load();
 
             //Console.WriteLine(employee?.Id ?? 0);
@@ -46,10 +59,44 @@ namespace EF03
             //Console.WriteLine(employee?.HiringDate ?? DateTime.Now);
             //Console.WriteLine(employee?.WorkFor?.Name ?? "NA");
 
-            var department = context.Departments.FirstOrDefault(d => d.Id == 100);
+            //var department = context.Departments.FirstOrDefault(d => d.Id == 100);
+
+            //context.Entry(department).Collection(D => D.Employees).Load(); // Explicit 
+
+            ////List or collection >> .Collection , normal >> .Reference
+
+            //Console.WriteLine(department.Name);
+
+            //foreach (var item in department.Employees)
+            //{
+            //    Console.WriteLine(item.Name);
+            //}
+
+            // 2. Eager Loading
+
+            //var employee = context.Employees.Include(E => E.WorkFor).FirstOrDefault(e => e.Id == 10);
 
 
+            //Console.WriteLine(employee?.Id ?? 0);
+            //Console.WriteLine(employee?.Name ?? "NA");
+            //Console.WriteLine(employee?.Salary ?? 0.0f);
+            //Console.WriteLine(employee?.Address ?? "NA");
+            //Console.WriteLine(employee?.DeptId ?? 0);
+            //Console.WriteLine(employee?.HiringDate ?? DateTime.Now);
+            //Console.WriteLine(employee?.WorkFor?.Name ?? "NA");
 
+
+            //var department = context.Departments.Include(D => D.Employees).FirstOrDefault(d => d.Id == 100);
+
+            //foreach (var item in department.Employees)
+            //{
+            //    Console.WriteLine(item.Name);
+            //}
+
+            //within the same query - one request (subquery join)
+
+
+           
 
 
         }
