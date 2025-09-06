04c030dd78e1cd095dc0ca694b1c3408cee88e25
Ahmed Adel Hassan <aa3452000@gmail.com>
2025-09-06 08:53:17 +0300

    V02

---
+++ InheiratenceExample/Contexts/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using InheiratenceExample.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheiratenceExample.Contexts
+{
+    internal class AppDbContext : DbContext
+    {
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //TPH 
+            //modelBuilder.Entity<FullTimeEmployee>().HasBaseType<Employee>();   
+            //modelBuilder.Entity<PartTimeEmployee>().HasBaseType<Employee>();
+
+            ////TPC
+            //modelBuilder.Entity<Employee>().ToTable("Employees");
+            //modelBuilder.Entity<FullTimeEmployee>().ToTable("FullTimeEmployees");
+            //modelBuilder.Entity<PartTimeEmployee>().ToTable("PartTimeEmployees");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = . ; Database = DB; Trusted_Connection = True ; TrustServerCertificate = True ");
+        }
+        //public DbSet<Employee> Employees {get; set;}
+        public DbSet<FullTimeEmployee> FullTimeEmployees { get; set; }
+        public DbSet<PartTimeEmployee> PartTimeEmployees { get; set; }
+    }
+}
