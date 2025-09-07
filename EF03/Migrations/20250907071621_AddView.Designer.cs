b7569c3511bfe9543b2674d6c407897bb3324824
Ahmed Adel Hassan <aa3452000@gmail.com>
2025-09-07 10:31:42 +0300

    V12

--- EF03/Migrations/20250907071621_AddView.Designer.cs
+++ EF03/Migrations/20250907071621_AddView.Designer.cs
@@ -4,6 +4,7 @@ using EF03.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EF03.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250907071621_AddView")]
+    partial class AddView
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EF03/Migrations/20250907071621_AddView.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF03.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddView : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"Create Or Alter View DepartmentMangersView
+                                    as  
+                                        Select E.Id as EmpId , E.Name , D.Id as DeptId , D.DeptName
+                                        From Employees E , Departments D 
+                                        Where E.Id = D.ManagerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"Drop View DepartmentMangersView");
+        }
+    }
+}
